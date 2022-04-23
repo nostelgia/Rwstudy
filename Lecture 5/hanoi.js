387c034513074f02293a30406a71ee1f6d64f498
nostelgia <seu3386@daum.net>
2022-04-23 21:27:42 +0900

    오늘 한것 아직 미완성

--- Lecture 5/hanoi.js
+++ Lecture 5/hanoi.js
@@ -1,5 +1,12 @@
-function Init() {
-    var div = document.getElementById('button_reset');
-    
-    div.remove();
-  } 
+function init(){
+    var arr = [];
+
+    arr.push("s");
+    arr.push("d");
+    arr.push("f");
+    arr.push("g");
+    arr.push("h");
+
+    console.log(arr.pop());
+    console.log(arr.shift());
+}
