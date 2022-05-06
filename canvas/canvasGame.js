794c271ae8d2cd7b35fafa130a115a019e4e099d
nostelgia <seu3386@daum.net>
2022-05-06 15:34:56 +0900

    캔버스 그리기

---
+++ canvas/canvasGame.js
@@ -0,0 +1,28 @@
+var canvas = document.getElementById('game');
+var context = canvas.getContext('2d');
+
+canvas.onmousedown = function(event) {
+        context.arc(event.x, event.y, 15, 0, Math.PI*2, true);
+        context.closePath();
+        context.fill();
+    }
+
+
+class player{
+    constructor(x,y,radius,color)
+    {
+        this.pos_x = x;
+        this.pos_y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+    draw()
+    {
+        context.arc(this.pos_x,this.pos_y,this.radius,0,2*Math.PI)
+        context.fillStyle = this.color;
+        context.fill();
+    }
+}
+
+var p = new player(100,100,30,'pink');
+p.draw();
